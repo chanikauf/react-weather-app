f420688ad826421ded2c92b7e9b3b3c1c513e5ff
Chani Kaufman <ckaufman@by.local>
2025-05-12 16:52:19 +0100

    Link to Github

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import "./styles.css"
-import React from 'react';
+import "./styles.css";
+import React from "react";
 import Weather from "./Weather";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <p>
+        This project was coded by Chani Kaufman and is open-sourced on{" "}
+        <a href="https://github.com/chanikauf/react-weather-app">Github</a>
+      </p>
     </div>
   );
 }
